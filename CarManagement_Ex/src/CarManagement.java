740239fff72e5ed45183f0e756b690e4cfd58a7a
C. Fulton <cfulton@luc.edu>
2025-02-26 21:52:55 -0600

    adding changes to carmanagement

--- CarManagement_Ex/src/CarManagement.java
+++ CarManagement_Ex/src/CarManagement.java
@@ -1,28 +1,33 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class CarManagement {
     public static void main(String[] args) throws Exception {
        
         Scanner input = new Scanner(System.in);
         String model;
         double price;
+        ElectricCar aCar;
 
-        ElectricCar [] electricCars = new ElectricCar[2]; //Array of Cars
+        ArrayList<ElectricCar> electricCars = new ArrayList<>(); //Array of Cars
 
 
         //Populates the electricCar Array
-        for(var i = 0; i < electricCars.length; i++){
+        for(var i = 0; i < electricCars.size(); i++){
             System.out.print("Please enter name of Car: ");
             model = input.nextLine();
 
             System.out.print("Please enter the price of Car: ");
             price = Double.parseDouble(input.nextLine());
-            electricCars[i] = new ElectricCar(model, price);
+            aCar = new ElectricCar(model, price);
+            
+            //Now Add Car instance on line 20 to the Arraylist
+            electricCars.add(aCar);
         }
 
        //Output the Electric Cars
        // for each loop 
-        for (Car aCar : electricCars) {
-            System.out.println(aCar);
+        for (Car x : electricCars) {
+            System.out.println(x);
     }
 
 
